c99d2e86c8e0b8085ac4029b9896f9f4474768b1
Oguz Ulgen <oulgen7@gmail.com>
2016-01-27 14:54:22 -0500

    Add starter code for assignment 2

---
+++ render/Makefile
@@ -0,0 +1,65 @@
+
+EXECUTABLE := render
+
+CU_FILES   := cudaRenderer.cu
+
+CU_DEPS    :=
+
+CC_FILES   := main.cpp display.cpp benchmark.cpp refRenderer.cpp \
+              noise.cpp ppm.cpp sceneLoader.cpp
+
+LOGS	   := logs
+
+###########################################################
+
+ARCH=$(shell uname | sed -e 's/-.*//g')
+OBJDIR=objs
+CXX=g++ -m64
+CXXFLAGS=-O3 -Wall -g
+HOSTNAME=$(shell hostname)
+
+LIBS       :=
+FRAMEWORKS := 
+
+ifeq ($(HOSTNAME), latedays.andrew.cmu.edu)
+# Building on Latedays
+NVCCFLAGS=-O3 -m64 -arch compute_20
+LIBS += GL glut cudart
+LDFLAGS=-L/usr/local/cuda/lib64/ -lcudart
+else
+# Building on Linux
+NVCCFLAGS=-O3 -m64 -arch compute_20
+LIBS += GL glut cudart
+LDFLAGS=-L/usr/local/depot/cuda-6.5/lib64/ -lcudart
+endif
+
+LDLIBS  := $(addprefix -l, $(LIBS))
+LDFRAMEWORKS := $(addprefix -framework , $(FRAMEWORKS))
+
+NVCC=nvcc
+
+OBJS=$(OBJDIR)/main.o $(OBJDIR)/display.o $(OBJDIR)/benchmark.o $(OBJDIR)/refRenderer.o \
+     $(OBJDIR)/cudaRenderer.o $(OBJDIR)/noise.o $(OBJDIR)/ppm.o $(OBJDIR)/sceneLoader.o
+
+
+.PHONY: dirs clean
+
+default: $(EXECUTABLE)
+
+dirs:
+		mkdir -p $(OBJDIR)/
+
+clean:
+		rm -rf $(OBJDIR) *~ $(EXECUTABLE) $(LOGS)
+
+check:	default
+		./checker.pl
+
+$(EXECUTABLE): dirs $(OBJS)
+		$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS) $(LDLIBS) $(LDFRAMEWORKS)
+
+$(OBJDIR)/%.o: %.cpp
+		$(CXX) $< $(CXXFLAGS) -c -o $@
+
+$(OBJDIR)/%.o: %.cu
+		$(NVCC) $< $(NVCCFLAGS) -c -o $@
---
+++ saxpy/saxpy.cu
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <driver_functions.h>
+
+#include "CycleTimer.h"
+
+extern float toBW(int bytes, float sec);
+
+__global__ void
+saxpy_kernel(int N, float alpha, float* x, float* y, float* result) {
+
+    // compute overall index from position of thread in current block,
+    // and given the block we are in
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (index < N)
+       result[index] = alpha * x[index] + y[index];
+}
+
+void
+saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) {
+
+    int totalBytes = sizeof(float) * 3 * N;
+
+    // compute number of blocks and threads per block
+    const int threadsPerBlock = 512;
+    const int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    float* device_x;
+    float* device_y;
+    float* device_result;
+
+    //
+    // TODO allocate device memory buffers on the GPU using cudaMalloc
+    //
+
+    // start timing after allocation of device memory
+    double startTime = CycleTimer::currentSeconds();
+
+    //
+    // TODO copy input arrays to the GPU using cudaMemcpy
+    //
+
+    // run kernel
+    saxpy_kernel<<<blocks, threadsPerBlock>>>(N, alpha, device_x, device_y, device_result);
+    cudaThreadSynchronize();
+
+    //
+    // TODO copy result from GPU using cudaMemcpy
+    //
+
+    // end timing after result has been copied back into host memory
+    double endTime = CycleTimer::currentSeconds();
+
+    cudaError_t errCode = cudaPeekAtLastError();
+    if (errCode != cudaSuccess) {
+        fprintf(stderr, "WARNING: A CUDA error occured: code=%d, %s\n", errCode, cudaGetErrorString(errCode));
+    }
+
+    double overallDuration = endTime - startTime;
+    printf("Overall: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * overallDuration, toBW(totalBytes, overallDuration));
+
+    // TODO free memory buffers on the GPU
+}
+
+void
+printCudaInfo() {
+
+    // for fun, just print out some stats on the machine
+
+    int deviceCount = 0;
+    cudaError_t err = cudaGetDeviceCount(&deviceCount);
+
+    printf("---------------------------------------------------------\n");
+    printf("Found %d CUDA devices\n", deviceCount);
+
+    for (int i=0; i<deviceCount; i++) {
+        cudaDeviceProp deviceProps;
+        cudaGetDeviceProperties(&deviceProps, i);
+        printf("Device %d: %s\n", i, deviceProps.name);
+        printf("   SMs:        %d\n", deviceProps.multiProcessorCount);
+        printf("   Global mem: %.0f MB\n",
+               static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024));
+        printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
+    }
+    printf("---------------------------------------------------------\n");
+}
